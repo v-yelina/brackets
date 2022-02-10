3c98f5b520768c126e94ac94c1ea7b6f570eb32d
v-yelina <heytridvaodin@gmail.com>
2022-02-10 10:58:45 +0100

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let newStr = str;
+  while (newStr.length !== 0 && count(newStr, bracketsConfig) !== 0)
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      let config = bracketsConfig[i].join("");
+      newStr = newStr.replace(config, "");
+    }
+  return newStr.length === 0;
+};
+
+const count = (str, bracketsConfig) => {
+  let result = 0;
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    let config = bracketsConfig[i].join("");
+    if (str.indexOf(config) !== -1) {
+      result += 1;
+    }
+  }
+  return result;
+};
